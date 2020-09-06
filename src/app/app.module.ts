6e36f77bfb10bdc5b1b5cc791558925c9f7c0db8
alhec <hector080896@gmail.com>
2020-09-06 18:08:57 -0400

    HTTP Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,11 +30,12 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { LoginComponent } from './login/login.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
-import { LoginComponent } from './login/login.component';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 import { baseURL } from './shared/baseurl';
 
@@ -76,7 +77,8 @@ import { baseURL } from './shared/baseurl';
     DishService,
     PromotionService,
     LeaderService,
-    {provide: 'BaseURL', useValue: baseURL}
+    {provide: 'BaseURL', useValue: baseURL},
+    ProcessHTTPMsgService
   ],
   entryComponents: [
     LoginComponent
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -20,6 +20,7 @@ export class DishdetailComponent implements OnInit {
     prev: string;
     next: string;
     comment: Comment;
+    errMess: string;
 
     @ViewChild('cform') commentFormDirective;
     commentForm: FormGroup;
@@ -53,7 +54,8 @@ export class DishdetailComponent implements OnInit {
         // .subscribe((dish) => this.dish = dish);
         this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
         this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-        .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+        .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); },
+        errmess => this.errMess = <any>errmess);
         }
 
     setPrevNext(dishId: string) {
